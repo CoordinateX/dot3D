e11b121994bf6451e58e05ec21cb6d6eb96d3ec6
ScrambledRK <w0rminf35t0r@gmx.net>
2013-12-15 22:18:44 +0100

    optional parameter for absolute path so it works on a remote server

--- build_doc.bat
+++ build_doc.bat
@@ -1,4 +1,9 @@
+@IF "%1"=="" (
+call build_doc.bat %~dp0doc 
+EXIT )
+@echo doc-directory: %1
+
 del "doc/." /s /q
 haxe --no-output -xml doc/at.xml build.hxml
-haxelib run dox -r %~dp0doc -o doc/ -i doc
+haxelib run dox -r %1 -o doc/ -i doc
 PAUSE
